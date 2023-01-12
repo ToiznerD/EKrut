832da6a64163b69daca829b1d501f0d27eb3edee
nave cohen <nave.cohen@e.braude.ac.il>
2023-01-12 12:11:08 +0200

    fixing config confilicts

--- Ekrut - client/src/controllers/ConnectionController.java
+++ Ekrut - client/src/controllers/ConnectionController.java
@@ -1,22 +1,16 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Optional;
-
 import Util.Msg;
 import Util.Tasks;
 import client.ClientBackEnd;
 import client.Config;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
 
 public class ConnectionController extends AbstractController {
