b090b0d67fc9f9851b4e86c674a887c863e4108d
Nave-Cohen <117146523+Nave-Cohen@users.noreply.github.com>
2023-01-15 21:49:06 +0200

    Nave (#41)
    * fixing small error
    
    * fix order,added db script functionality
    
    * paymentScreen added, insert added to tasker,
    
    * orderdetails Entity fixed
    
    * changed after zoom meeting,db
    
    * customer panel change
    
    * fix timer bug
    
    * customer panel change
    
    * last fix

--- Ekrut - client/src/controllers/CustomerPanelController.java
+++ Ekrut - client/src/controllers/CustomerPanelController.java
@@ -10,90 +10,89 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class CustomerPanelController extends AbstractController {
-	
-    @FXML
-    private Button btnLogout;
 
-    @FXML
-    private Button btnMakeOrder;
-    
-    @FXML
-    private Button btnPickup;
-    
-    @FXML
-    private Button btnApproveDelivery;
-    
-    @FXML
-    private Label welcomeLbl;
-    
-    @FXML
-    private Label errLbl;
+	@FXML
+	private Button btnLogout;
 
-    /**
-     * Initializes the panel based on the user that logged in, whether he's an approved/not approved or a registered/not registered customer.
-     */
-    @FXML
-    public void initialize() {
-    	//Editing welcome label
-    	String welcome = welcomeLbl.getText() + " ";
-    	welcome = myUser.getName() == null ? welcome + myUser.getUsername() : welcome + myUser.getName();
-    	welcomeLbl.setText(welcome);
-    	
-    	//Get the customer status and check if he is subscriber
+	@FXML
+	private Button btnMakeOrder;
+
+	@FXML
+	private Button btnPickup;
+
+	@FXML
+	private Button btnApproveDelivery;
+
+	@FXML
+	private Label welcomeLbl;
+
+	@FXML
+	private Label errLbl;
+
+	/**
+	 * Initializes the panel based on the user that logged in, whether he's an approved/not approved or a registered/not registered customer.
+	 */
+	@FXML
+	public void initialize() {
+		//Editing welcome label
+		String welcome = welcomeLbl.getText() + " ";
+		welcome = myUser.getName() == null ? welcome + myUser.getUsername() : welcome + myUser.getName();
+		welcomeLbl.setText(welcome);
+
+		//Get the customer status and check if he is subscriber
 		String customerQuery = "SELECT status, subscriber FROM customer WHERE id = " + myUser.getId();
 		msg = new Msg(Tasks.Select, customerQuery);
 		sendMsg(msg);
-    	
-    	
-    	if(myUser.getRole().equals("new_user") || !msg.getBool()) {
-    		//If a new user / not a customer
-    		errLbl.setText("You need to register as a customer to continue.");
-    	}
-    	
-    	else {
-    		//Customer
-			if(msg.getObj(0).equals("Not Approved")) {
+
+		if (myUser.getRole().equals("new_user") || !msg.getBool()) {
+			//If a new user / not a customer
+			errLbl.setText("You need to register as a customer to continue.");
+		}
+
+		else {
+			//Customer
+			if (msg.getObj(0).equals("Not Approved")) {
 				//Customer not approved
-	    		errLbl.setText("Your account has not been approved yet.");
+				errLbl.setText("Your account has not been approved yet.");
 			}
-			
+
 			else {
 				//Customer approved
-				if(Config.getConfig().equals("OL")) {
+				if (Config.getConfig().equals("OL")) {
 					//OL configuration
-					if((int)(msg.getObj(1)) == 0) {
+
+					if ((int) (msg.getObj(1)) == 0) {
+
 						//Regular customer
 						errLbl.setText("You need to be a subscriber to login here.");
-					}
-					else {
+					} else {
 						//Subscriber
 						btnMakeOrder.setVisible(true);
 						btnApproveDelivery.setVisible(true);
 						errLbl.setDisable(true);
 					}
-				}
-				else {
+				} else {
 					//EK configuration
 					btnMakeOrder.setVisible(true);
 					//Subscriber customer
-					
-					btnPickup.setVisible((int)(msg.getObj(1)) == 1);
+
+					btnPickup.setVisible((int) (msg.getObj(1)) == 1);
 					errLbl.setDisable(true);
 				}
 			}
-    	}
-    }
-    
-    /**
-     * Handles the mouse event of the back button.
-     * 
-     * @param event the mouse event that triggered this method
-     */
+		}
+	}
+
+	/**
+	 * Handles the mouse event of the back button.
+	 * 
+	 * @param event the mouse event that triggered this method
+	 */
 	@Override
 	public void back(MouseEvent event) {
 		// not implemented
 	}
-	
+
 	/**
 	* The MakeOrder method is used to open the order form or order screen, depending on the system configuration.
 	* If the configuration is OL, it opens the "OrderMethodForm" window.
@@ -101,46 +100,45 @@ public class CustomerPanelController extends AbstractController {
 	*/
 	@FXML
 	public void MakeOrder(ActionEvent event) {
-		if(Config.getConfig().equals("OL")) {
+		if (Config.getConfig().equals("OL")) {
 			try {
-				start("OrderMethodForm","Order Method Form");
+				start("OrderMethodForm", "Order Method Form");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
-		else {
+		} else {
 			try {
-				start("OrderScreen","Order Screen");
+				start("OrderScreen", "Order Screen");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	/**
 	* The PickupOrder method is used to open the pickup order form,
 	*/
 	@FXML
 	public void PickupOrder(ActionEvent event) {
 		try {
-			start("PickupForm","Pickup Order Form");
+			start("PickupForm", "Pickup Order Form");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	* The ApproveDelivery method is used to open the customer deliveries form,
 	*/
 	@FXML
-	public void ApproveDelivery (ActionEvent event) {
+	public void ApproveDelivery(ActionEvent event) {
 		try {
-			start("CustomerDeliveriesForm","Customer Deliveries Form");
+			start("CustomerDeliveriesForm", "Customer Deliveries Form");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void setUp(Object... objects) {
 		// Not implemented
