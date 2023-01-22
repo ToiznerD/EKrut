dd2874637050af99af1811a48c6ac2a798d98211
נתנאל איוב <nati16368447@gmail.com>
2023-01-22 18:49:45 +0200

    stockStatusReportController update

--- Ekrut - client/UnitTests/controllers/StockStatusReportControllerTest.java
+++ Ekrut - client/UnitTests/controllers/StockStatusReportControllerTest.java
@@ -24,7 +24,6 @@ public class StockStatusReportControllerTest {
 			
 			stockReportController.setStockReportToDisplay(reportInDb);
 		}
-	
 	}
 	
 	@BeforeEach
--- Ekrut - client/src/controllers/IReportService.java
+++ Ekrut - client/src/controllers/IReportService.java
@@ -4,7 +4,5 @@ import Entities.StockReport;
 
 public interface IReportService {
 	 public void getReportFromDb(String storeLocation, String month, String year);
-     //public void setStockReport(StockReport stockReport);
 	 
-
 }
